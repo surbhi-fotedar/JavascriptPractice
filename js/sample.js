d9ee85b14fedc0036239689c25a308110bdbead1
Surbhi Fotedar <surbhif@CESCHN.COM>
2018-07-30 19:05:02 +0530

    operators

--- js/sample.js
+++ js/sample.js
@@ -1,9 +1,19 @@
 (function() {
+    'use strict';
     var str = Number("an arbitrary string");
     console.log(typeof str);
 
     let num = Number(str);
 
     console.log(typeof num);
+    console.log(Boolean("0"));
+    console.log(Boolean(" "));
+    console.log(2 + 2 + '1' - 2 + 3 + '1');
+    var x = 2;
+    var y = '3';
+    console.log(+x + +y);
+    console.log(--x);
+    console.log(--y);
+
 
 })();
