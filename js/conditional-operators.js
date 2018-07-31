9795d8ba41f40de858004ef58958175c4f2a6004
Surbhi Fotedar <surbhif@CESCHN.COM>
2018-07-31 14:00:57 +0530

    if else and ternary conditional operators

---
+++ js/conditional-operators.js
@@ -0,0 +1,16 @@
+(function() {
+  'use strict';
+  var year = prompt('In which year was ECMAScript-2015 specification published?', '');
+
+  if (year < 2015) {
+    alert('Too early...');
+  } else if (year > 2015) {
+    alert('Too late');
+  } else {
+    alert('Exactly!');
+  }
+  var age = prompt('how old are you?', '');
+  var accessAllowed = (age > 18) ? true : false;
+  alert(accessAllowed);
+
+})();
--- js/sample.js
+++ js/sample.js
@@ -1,28 +1,31 @@
 (function() {
-    'use strict';
-    var str = Number("an arbitrary string");
-    console.log(typeof str);
 
-    let num = Number(str);
+  'use strict';
 
-    console.log(typeof num);
-    console.log(Boolean("0"));
-    console.log(Boolean(" "));
-    console.log(2 + 2 + '1' - 2 + 3 + '1');
-    var x = 2;
-    var y = '3';
-    console.log(+x + +y);
-    console.log(--x);
-    console.log(--y);
-    console.log('Zee' < 'Zeee');
-    console.log('true' != 1);
-    var b = '0'
-    var a = 0;
-    console.log(a === b); //strict equality operator checks equality without type conversion
+  var str = Number('an arbitrary string');
+  console.log(typeof str);
 
-    console.log(null == undefined);
-    console.log(typeof(null));
-    console.log(typeof(undefined));
+  var num = Number(str);
 
+  console.log(typeof num);
+  console.log(Boolean('0'));
+  console.log(Boolean(''));
+  console.log(2 + 2 + '1' - 2 + 3 + '1');
+  var x = 2;
+  var y = '3';
+
+  /* jshint -W007 */
+  console.log(+x + +y);
+  console.log(--x);
+  console.log(--y);
+  console.log('Zee' < 'Zeee');
+  console.log('true' != 1);
+  var b = '0';
+  var a = 0;
+  console.log(a === b); //strict equality operator checks equality without type conversion
+
+  console.log(null == undefined);
+  console.log(typeof(null));
+  console.log(typeof(undefined));
 
 })();
