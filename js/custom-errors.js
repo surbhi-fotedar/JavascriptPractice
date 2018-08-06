aa281c387bfb5ff5b588981fe25a89f2735d4e87
Surbhi Fotedar <surbhif@CESCHN.COM>
2018-08-06 18:38:07 +0530

    Custom-errors in ES6

---
+++ js/custom-errors.js
@@ -0,0 +1,38 @@
+(function() {
+  'use strict';
+  /* jshint -W104 */
+  class ValidationError extends Error {
+    constructor(message) {
+      super(message);
+      this.name = "ValidationError";
+    }
+  }
+
+  // Usage
+  function readUser(json) {
+    var user = JSON.parse(json);
+
+    if (!user.age) {
+      throw new ValidationError("No field: age");
+    }
+    if (!user.name) {
+      throw new ValidationError("No field: name");
+    }
+
+    return user;
+  }
+
+  // Working example with try..catch
+
+  try {
+    var user = readUser('{ "age": 25 }');
+  } catch (err) {
+    if (err instanceof ValidationError) {
+      alert("Invalid data: " + err.message); // Invalid data: No field: name
+    } else if (err instanceof SyntaxError) { // (*)
+      alert("JSON Syntax Error: " + err.message);
+    } else {
+      throw err; // unknown error, rethrow it (**)
+    }
+  }
+})();
